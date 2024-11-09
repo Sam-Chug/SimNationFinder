a5d316f7c5c2c8aa8ac3435558a5abe9fcd2fac4
Sam-Chug <sfackler96@gmail.com>
2024-11-08 23:02:27 -0500

    Incremented version

--- js/definitions.js
+++ js/definitions.js
@@ -1,5 +1,5 @@
 //#region Strings
-const VERSION_STR = "v0.2.3f";
+const VERSION_STR = "v0.2.3g";
 
 //http://simnationserver.com:9000/userapi/avatars/2009
 //http://api.freeso.org/userapi/city/1/lots/online
@@ -282,10 +282,10 @@ const CONFETTI_DATA = {
 const CUSTOM_STYLE_REAGAN = "Reagan Lambo";
 
 const CUSTOM_STYLE_SIMHEADS = {
-    male: "./images/sim-faces/simface-m.png?v0.2.3f",
-    female: "./images/sim-faces/simface-f.png?v0.2.3f",
-    reagan: "./images/sim-faces/simface-rea.png?v0.2.3f",
-    bear: "./images/sim-faces/simface-b.png?v0.2.3f"
+    male: "./images/sim-faces/simface-m.png?v0.2.3g",
+    female: "./images/sim-faces/simface-f.png?v0.2.3g",
+    reagan: "./images/sim-faces/simface-rea.png?v0.2.3g",
+    bear: "./images/sim-faces/simface-b.png?v0.2.3g"
 };
 const CUSTOM_STYLE_BLOCK = {
     bp: {
