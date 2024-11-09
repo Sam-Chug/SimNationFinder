ddd84d201e1b69bdee671998ac4160818f583650
Sam-Chug <sfackler96@gmail.com>
2024-11-08 23:12:46 -0500

    Increment version

--- js/definitions.js
+++ js/definitions.js
@@ -1,5 +1,5 @@
 //#region Strings
-const VERSION_STR = "v0.2.3h";
+const VERSION_STR = "v0.2.3i";
 
 //http://simnationserver.com:9000/userapi/avatars/2009
 //http://api.freeso.org/userapi/city/1/lots/online
@@ -282,10 +282,10 @@ const CONFETTI_DATA = {
 const CUSTOM_STYLE_REAGAN = "Reagan Lambo";
 
 const CUSTOM_STYLE_SIMHEADS = {
-    male: "./images/sim-faces/simface-m.png?v0.2.3h",
-    female: "./images/sim-faces/simface-f.png?v0.2.3h",
-    reagan: "./images/sim-faces/simface-rea.png?v0.2.3h",
-    bear: "./images/sim-faces/simface-b.png?v0.2.3h"
+    male: "./images/sim-faces/simface-m.png?v0.2.3i",
+    female: "./images/sim-faces/simface-f.png?v0.2.3i",
+    reagan: "./images/sim-faces/simface-rea.png?v0.2.3i",
+    bear: "./images/sim-faces/simface-b.png?v0.2.3i"
 };
 const CUSTOM_STYLE_BLOCK = {
     bp: {
